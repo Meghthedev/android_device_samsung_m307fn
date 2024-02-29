eb3874bfb4e9268688249523b0b739382548bc17
Meghthedev <meghthedev@gmail.com>
2024-02-29 18:55:30 +0530

    m307fn: PE Bringup
    * Adapt Tree for PE

--- aosp_m307fn.mk
+++ aosp_m307fn.mk
@@ -23,7 +23,6 @@ $(call inherit-product, device/samsung/m307fn/device.mk)
 
 ## Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
@@ -31,14 +30,14 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 
 ## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 ## Device identifier, this must come after all inclusions
 PRODUCT_DEVICE := m307fn
-PRODUCT_NAME := lineage_m307fn
+PRODUCT_NAME := aosp_m307fn
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-M307FN
 PRODUCT_MANUFACTURER := samsung
